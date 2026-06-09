92ea6310170af421854563457a34b98c42cba0cb
fayyoz24 <fayyozusmon@gmail.com>
2026-06-10 01:12:48 +0500

    added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a5grgtd)*vpwpe4!u0rmcd=29bi^+9e9ww5n-xsm866nl8b$vg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
  
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
  
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=lambda v: [s.strip() for s in v.split(',')])
  
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -94,6 +94,8 @@ USE_I18N = True
 USE_TZ = True
  
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
  
